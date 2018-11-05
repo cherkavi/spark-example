f6d18452edb1217e3cb97d012ea57a1756f0bdc3
technik <technik7job@gmail.com>
2018-11-05 07:02:47 +0100

    read different data types

---
+++ read-file.scala
@@ -0,0 +1,18 @@
+// parquet
+val parkuetDF = spark.read.load("/path/to/file/users.parquet")
+parkuetDF.select("name", "color").write.save("new_file.parquet")
+
+// json
+spark.read.format("json").load("examples/src/main/resources/people.json")
+
+// csv
+val csvDF = spark.read.format("csv")
+.option("sep", ";")
+.option("inferSchema", "true")
+.option("header", "true")
+.load("/path/to/file/people.csv")
+
+// orc
+sqlContext.read.format("orc").load(orcfile)
+spark.read.option("inferSchema", true).orc("filepath")
+spark.read.format("org.apache.spark.sql.execution.datasources.orc")
