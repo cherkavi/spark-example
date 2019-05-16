c5d072b97f3e5a8bf05eff5bf7996ac16fa06e11
vitaliicherkashyn <vitalii.cherkashyn@bmw.de>
2019-05-16 07:23:32 +0200

    user defined aggregated function

--- udaf.scala
+++ udaf.scala
@@ -26,37 +26,42 @@ class AverageTime extends UserDefinedAggregateFunction {
   override def inputSchema: org.apache.spark.sql.types.StructType = StructType(StructField("time", IntegerType) :: Nil)
 
   // This is the internal fields you keep for computing your aggregate.
-  override def bufferSchema: StructType = StructType(StructField("elements", ArrayType(LongType, false)) :: StructField("count", LongType) :: Nil)
+  override def bufferSchema: StructType = StructType(StructField("elements", ArrayType(IntegerType, false)) :: StructField("count", IntegerType) :: Nil)
 
   // output of aggregatation function.
-  override def dataType: DataType = LongType
+  override def dataType: DataType = DoubleType
 
   override def deterministic: Boolean = true
 
   // init 
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
-    buffer(0) = Array[Long]()
+    buffer(0) = Array[Integer]()
     buffer(1) = 0
   }
 
   // next element in group
-  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-    buffer(0) = buffer.getAs[Array[Long]](0) :+ input.getAs[Long](0)
+  override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {    
+    buffer(0) = buffer.getAs[Array[Integer]](0).toArray :+ input.getAs[Integer](0).toArray
     buffer(1) = buffer.getAs[Integer](1) + 1
   }
 
   // merge with another row ( from another RDD )
   override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-    buffer1(0) = buffer1.getAs[Array[Long]](0) ++ buffer1.getAs[Array[Long]](0)
+    buffer1(0) = buffer1.getAs[Array[Integer]](0) ++ buffer1.getAs[Array[Integer]](0)
     buffer1(1) = buffer1.getAs[Integer](1) + buffer2.getAs[Integer](1)
   }
 
   // This is where you output the final value, given the final value of your bufferSchema.
-  override def evaluate(buffer: Row): Long = {
-  	buffer.getAs[Array[Long]](0).sum/buffer.getAs[Integer](1)
+  override def evaluate(buffer: Row): Double = {
+  	val allElements = buffer.getAs[Array[Integer]](0)
+    var sum:Double = 0
+    for(each <- allElements){
+      sum+=each
+    }
+    sum/buffer.getAs[Integer](1)
   }
-
 }
+
 val groupping = new AverageTime
 dataFrame.groupBy("name").agg(groupping(col("time"))).show()
 
